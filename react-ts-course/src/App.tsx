cac16e1f3bfda34da37d586f4ac809b7cc877252
Satria Wibowo <extencezone02@gmail.com>
2022-07-26 11:16:52 +0700

    fist time

--- react-ts-course/src/App.tsx
+++ react-ts-course/src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./App.css";
 
-let name :string = "samurai"
+//## react ts
+let name: string = "samurai";
+let age: number;
+let isStudent: boolean;
+//string of array
+let hobbies: string[];
+//can contain 1 number and 1 string
+let role: [number, number, string];
+role = [4, 5, "gg"];
 
-function App(): JSX.Element  {
+function App(): JSX.Element {
     return (
         <div className="App">
             <h3>Hello asdsadworlsdfsdfd push</h3>
