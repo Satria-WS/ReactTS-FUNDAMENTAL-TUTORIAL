b054ef5b38990d2c0ae2d5e2e10a51d54a3f1061
Satria Wibowo <extencezone02@gmail.com>
2022-07-26 10:39:22 +0700

    first time

---
+++ react-ts-course/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+let name :string = "samurai"
+
+function App(): JSX.Element  {
+    return (
+        <div className="App">
+            <h3>Hello asdsadworlsdfsdfd push</h3>
+            <h3>{name}</h3>
+        </div>
+    );
+}
+
+export default App;
